e5bab3907245f44dd7072fffd2917a6f96f8524c
Rebekah <rebekahjanedejong@gmail.com>
2021-01-29 22:11:15 -0500

    Add AOS library to trigger animations on scrolling for the Title

--- src/Components/Title/index.js
+++ src/Components/Title/index.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 import './title.css'
 
 function Title({ text }) {
+    // might not need this useEffect, could probably just use AOS.init in componentDidMount 
+    useEffect(() => {
+        Aos.init({duration: 1000})
+    }, [])
     return (
         <div>
-            <h1 className="title">{text}</h1>
-            <hr className="blue" />
-            <hr className="yellow" />
-            <hr className="red" />
+            <h1 data-aos="fade-down" className="title">{text}</h1>
+            <hr data-aos="line-grows" className="blue" />
+            <hr data-aos="line-grows" data-aos-delay="100" className="yellow" />
+            <hr data-aos="line-grows" data-aos-delay="200" className="red" />
         </div>
     )
 }
